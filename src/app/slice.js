c0c529214c47dc0432c72934923bbf1a9c3ca292
farruop <rafayKhanpvt@gmail.com>
2024-11-09 16:47:49 +0530

    Implement Redux slice and asyncThunks for application and course management

--- src/app/slice.js
+++ src/app/slice.js
@@ -5,14 +5,21 @@ export const getApplications = createAsyncThunk("getApplications", async () => {
   /* Get applications of all user  
      Use url-  /api/applications with GET method */
 
+     const response = await axios.get("/api/applications")
+     return response.data;
+
 });
 
 export const modifyApplicationStatus = createAsyncThunk(
   "modifyApplicationStatus",
-  async (args) => {
+  async ({id, newStatus}) => {
     /* To modify the status of Application  
        Use url-  /api/applications/${id} with PATCH method 
        Req body - { status: <newStatus> },  newstatus can be Approved/Rejected*/
+
+       const response = await axios.patch(`/api/applications/${id}`, {status:newStatus});
+       return response.data;
+
 
 });
 
@@ -20,13 +27,18 @@ export const getCourses = createAsyncThunk("getCourses", async () => {
    /* Get all courses offered 
      Use url-  /api/courses with GET method */
 
+     const response = await axios.get("/api/courses")
+     return response.data;
+
 });
 
-export const addSeats = createAsyncThunk("addSeats", async (args) => {
+export const addSeats = createAsyncThunk("addSeats", async (id, updatedSeats) => {
   /* To modify seatCount of Course 
     Use url-  /api/courses/${id} with PATCH method 
     Req body - { availableSeats: <updatedSeatCount> } */
 
+    const response = await axios.patch(`/api/courses/${id}`, {availableSeats:updatedSeats})
+    return response.data
 });
 
 export const getApplicationStatus = createAsyncThunk(
@@ -34,11 +46,15 @@ export const getApplicationStatus = createAsyncThunk(
   async (args) => {
     /* Get applications of logged user  
      Use url-  /api/applications?applicantEmail=${email} with GET method */
+
+     const response= await axios.get(`/api/applications?applicantEmail=${email}`)
+     return response.data;
   }
 );
 
 const initialState = {
-  courses: []
+  courses: [],
+  applications:[]
 };
 
 export const slice = createSlice({
@@ -52,15 +68,15 @@ export const slice = createSlice({
 
   extraReducers: (builder) => {
     builder.addCase(getApplications.fulfilled, (state, action) => {
-      
+      state.applications= action.payload;
     });
 
     builder.addCase(getApplicationStatus.fulfilled, (state, action) => {
-      
+      state.applications=action.payload;
     });
 
     builder.addCase(getCourses.fulfilled, (state, action) => {
-      
+      state.courses=action.payload;
     });
   },
 });

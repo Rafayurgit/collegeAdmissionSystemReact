512d47dff4eebd7c177ab73e5f48b034e9b334aa
farruop <rafayKhanpvt@gmail.com>
2024-11-09 17:21:22 +0530

    Implement fetching and displaying user's application status with Redux and API call

--- src/pages/ApplicationStatus.js
+++ src/pages/ApplicationStatus.js
@@ -8,13 +8,25 @@ function ApplicationStatus() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const email = useSelector((state)=> state.user.email)
+  const applications= useSelector((state)=> state.admission.applications)
+
+  useEffect(()=>{
+    if(email && applications.length===0){
+      dispatch(getApplicationStatus(email))
+    }
+  },[email, dispatch, applications.length])
+
+
   return (
     <div className="container">
       <h3>Your Applications</h3>
       {/* If user haven't applied for any course*/}
-      <p>You have not applied for any course.</p>
+      
       {/* If any application is available, display data in table*/}
-      <table className="table table-hover">
+      { applications.length==0 ? (<p>You have not applied for any course.</p>
+      ):(
+       <table className="table table-hover">
         <thead>
           <tr>
             <th scope="col">Application Id</th>
@@ -32,6 +44,8 @@ function ApplicationStatus() {
           </tr>
         </tbody>
       </table>
+    )}
+      
     </div>
   );
 }
